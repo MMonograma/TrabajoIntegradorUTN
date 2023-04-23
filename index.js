a8045efd8e1c58407fe08b371c02a639538cf93e
MMonograma <Leandrix92@gmail.com>
2023-04-23 18:28:18 -0300

    Se acomodo el JS y el HTML para que muestre todas las imagenes, y recetas

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-//Direccion de API como constante para que no se cambie
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
 
-// Obtener el nombre del cóctel del HTML
-const cocktailName = document.getElementById("cocktail-name").textContent;
+const cocktails = ['margarita', 'martini', 'daiquiri', 'cosmopolitan', 'mojito'];
 
-// Concatenar el nombre del cóctel con la URL de la API
-const fullUrl = url + cocktailName;
+cocktails.forEach((cocktail, index) => {
+  const cocktailName = document.getElementById(`cocktail-name${index + 1}`);
+  const cocktailImage = document.getElementById(`cocktail-image${index + 1}`);
+  const cocktailRecipe = document.getElementById(`cocktail-recipe${index + 1}`);
 
-// Hacer la llamada fetch Una Promesa de Get
-fetch(fullUrl)
-  .then(response => response.json())
-  .then(data => {
-    const cocktail = data.drinks[0];
-
-    // Mostrar imagen
-    const cocktailImage = document.getElementById("cocktail-image");
-    cocktailImage.src = cocktail.strDrinkThumb;
-
-    // Mostrar receta
-    const cocktailRecipe = document.getElementById("cocktail-recipe");
-    cocktailRecipe.textContent = cocktail.strInstructions;
-  })
-  .catch(error => console.error(error));
+  fetch(`${baseUrl}search.php?s=${cocktail}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const drink = data.drinks[0];
+      cocktailName.innerText = drink.strDrink;
+      cocktailImage.src = drink.strDrinkThumb;
+      cocktailRecipe.innerText = drink.strInstructions;
+    })
+    .catch((error) => console.log(error));
+});
